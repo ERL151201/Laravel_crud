fc608d624816e8e417d3c7969cd8837f9a900309
Emanuel Rangel <emanuelrangelluna@gmail.com>
2021-02-07 20:16:29 -0600

    logica view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,23 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     public function index()
     {
+        $users = User::latest()->get();
 
+        return view('users.index',[
+            'users' => $users
+        ]);
     }
 
-    public function store()
+    public function store(Request $request)
     {
+    User::create([
+        'name' => $request->name,
+        'email' => $request->email,
+        'password' => $request->password,
 
+        ]);
+
+        return back();
     }
 
-    public function destroy()
+    public function destroy(User $user)
     {
+        $user->delete();
 
+        return back();
     }
 }
